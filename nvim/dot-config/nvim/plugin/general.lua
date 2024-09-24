fff278e23bbc7116a291d850040ee0d63d709ebb
Kenny Parnell <k.parnell@gmail.com>
2024-09-24 12:51:20 -0400

    Don't create undofile for `/tmp/` files.

--- nvim/dot-config/nvim/plugin/general.lua
+++ nvim/dot-config/nvim/plugin/general.lua
@@ -2,3 +2,10 @@ local data = assert(vim.fn.stdpath("data")) --[[@as string]]
 
 vim.opt.undodir = vim.fs.joinpath(data, "undodir")
 vim.opt.undofile = true
+
+local undogroup = vim.api.nvim_create_augroup("undogroup", { clear = true })
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = "/tmp/*",
+  group = undogroup,
+  command = "setlocal noundofile",
+})
