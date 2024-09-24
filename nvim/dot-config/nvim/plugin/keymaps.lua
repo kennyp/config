dd548a6c8f5721d93f4a93abe52a0a361ef8bbe5
Kenny Parnell <k.parnell@gmail.com>
2024-09-24 16:42:00 -0400

    Add keymaps for copy/paste from system clipboard.

--- nvim/dot-config/nvim/plugin/keymaps.lua
+++ nvim/dot-config/nvim/plugin/keymaps.lua
@@ -1,2 +1,31 @@
+-- Window Stuff
 vim.keymap.set("n", "<left>", vim.cmd.tabp)
 vim.keymap.set("n", "<right>", vim.cmd.tabn)
+
+-- Copy and paste using external clipboard
+vim.keymap.set("n", "<leader>y", '"+y', { desc = "yank to system clipboard", silent = true })
+vim.keymap.set("v", "<leader>Y", '"+Y', { desc = "yank to system clipboard", silent = true })
+vim.keymap.set(
+  "n",
+  "<leader>p",
+  ':set paste<CR>"+p:set nopaste<CR>',
+  { desc = "put after cursor from system clipboard", silent = true }
+)
+vim.keymap.set(
+  "v",
+  "<leader>p",
+  'x:set paste<CR>"+p:set nopaste<CR>',
+  { desc = "put after cursor from system clipboard", silent = true }
+)
+vim.keymap.set(
+  "n",
+  "<leader>P",
+  ':set paste<CR>"+P:set nopaste<CR>',
+  { desc = "put before cursor from system clipboard", silent = true }
+)
+vim.keymap.set(
+  "v",
+  "<leader>P",
+  'x:set paste<CR>"+P:set nopaste<CR>',
+  { desc = "put before cursor from system clipboard", silent = true }
+)
