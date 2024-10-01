aa63df7233aedda919e9fe48a1dd4b6b9f49513a
Kenny Parnell <k.parnell@gmail.com>
2024-10-01 08:14:26 -0400

    Bump default Go to 1.23.1

---
+++ nvim/dot-config/nvim/after/ftplugin/go.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<leader>t", function()
+  local cmd = (":!go test ./%s/...<cr>").format(vim.fn.expand("%:h"))
+  vim.cmd(cmd)
+end, { desc = "run test for current package", buffer = true })
