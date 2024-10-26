fee213b364c313224f4b7ca3ddc947308d4bf519
Kenny Parnell <k.parnell@gmail.com>
2024-10-26 11:49:10 -0400

    HACK: Include path in wezterm config.

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -1,9 +1,24 @@
 -- Imports and Globals
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local path = {
+	wezterm.home_dir .. "/bin",
+	wezterm.home_dir .. "/.cargo/bin",
+	"/opt/homebrew/bin",
+	"/usr/local/bin",
+	wezterm.home_dir .. "/.asdf/shims",
+	wezterm.home_dir .. "/.asdf/bin",
+	"/usr/bin",
+	"/bin",
+	"/usr/sbin",
+	"/sbin",
+}
 
 -- Basics
 config.default_prog = { "zellij", "-l", "welcome" }
+config.set_environment_variables = {
+	PATH = table.concat(path, ":"),
+}
 
 -- Appearance
 config.background = {
