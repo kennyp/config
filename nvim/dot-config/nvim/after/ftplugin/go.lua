c83ded011434457170998dac81fa6a97d6a8d41b
Kenny Parnell <k.parnell@gmail.com>
2024-10-28 11:03:00 -0400

    Set ts/sw for Go.

--- nvim/dot-config/nvim/after/ftplugin/go.lua
+++ nvim/dot-config/nvim/after/ftplugin/go.lua
@@ -2,3 +2,6 @@ vim.keymap.set("n", "<leader>t", function()
   local cmd = (":!go test ./%s/...<cr>").format(vim.fn.expand("%:h"))
   vim.cmd(cmd)
 end, { desc = "run test for current package", buffer = true })
+
+vim.bo.tabstop = 4
+vim.bo.shiftwidth = 4
